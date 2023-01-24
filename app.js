49c7567d6d87f8b99f8061c6a3486a8a09d27bec
Jongbo Im <whdqh1550@gmail.com>
2023-01-23 22:30:54 -0600

    router into different files

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const path = require("path");
 
 const express = require("express");
+
+const defaultRouter = require("./routes/default");
+
 const app = express();
 
 app.use(express.static("public"));
@@ -9,8 +12,6 @@ app.use(express.urlencoded({ extended: false }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", function (req, res) {
-  res.render("index");
-});
+app.use("/", defaultRouter);
 
 app.listen(1550);
