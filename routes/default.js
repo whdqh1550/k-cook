d5f269218018c0df6fe4874107e81529261cb7bb
Jongbo Im <whdqh1550@gmail.com>
2023-02-06 18:59:06 -0600

    multer config directory

--- routes/default.js
+++ routes/default.js
@@ -5,7 +5,7 @@ const db = require("../database/database");
 
 const storageConfig = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "images");
+    cb(null, "./images/");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
