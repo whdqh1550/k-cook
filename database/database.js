2d8024303bec907f0c1e53e79c44e1135cf0942c
Jongbo Im <whdqh1550@gmail.com>
2023-02-27 00:21:07 -0600

    undo db

--- database/database.js
+++ database/database.js
@@ -2,26 +2,14 @@ const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
-let client;
 let database;
 
 async function connect() {
-  if (client && client.isConnected()) {
-    // If there's already a connected client, reuse it
-    database = client.db();
-  } else {
-    // If not, create a new client and connect to the database
-    client = await MongoClient.connect(
-      "mongodb+srv://chungpodo:C5UOdgTJJejarxi8@cluster0.8eqm44w.mongodb.net/",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-    database = client.db("kCook");
-  }
+  const client = await MongoClient.connect(
+    "mongodb+srv://chungpodo:C5UOdgTJJejarxi8@cluster0.8eqm44w.mongodb.net/?retryWrites=true&w=majority"
+  );
+  database = client.db("kCook");
 }
-
 function getDB() {
   if (!database) {
     throw { message: "Db connection incomplete" };
